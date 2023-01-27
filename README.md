f3e4d8cf740765eb6eb8186c608a94446e7be428
paqstd <zebartcoc@gmail.com>
2023-01-27 15:07:02 +0300

    Added cobra & viper for cli. Added simple logic

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # Fake-Cli
+
+## About
+Generate fake-data and create local web-server. 
+
+Cli options:  
+- fake  
+Run cli with default options.  
+
+- fake -p 4321  
+Run cli with custom port `localhost:4321`. By default listen `localhost:8765`.
+
+- fake -c custom.json  
+Run cli with custom file configuration. By default config file is `fake.json`. 
+
+
+## Config file
+### Example fake.json file
+```
+{
+    "endpoints": {
+        "users/": {
+            "response": "some",
+            "scheme": {
+                "name": "name",
+                "surname": "phrase"
+            }
+        },
+        "users/1/": {
+            "response": "single",
+            "scheme": {
+                "name": "name"
+            }
+        }
+    }
+}
+```
+
+- endpoints:  
+An object of all endpoints to run the web server.
+
+- - key:  
+Each key is a path in the browser where one or another endpoint can be requested
+
+- - - response:  
+Points to the return type. It can be "single" or "some".
+
+- - - count:  
+If response == "some" is specified. Specifies how many records to return.
+
+- - - scheme:  
+The schema of the returned data. Fake data will be generated for the specified fields. Each key is a field name, each value is a generated type.
+
+
+### Available schema type values
+- name  
+Generation of first and last name. Example: `Markus Moen`.
+
+- phrase  
+Random phrase generation. Example: `head in the sand`.
--- main.go
+++ main.go
@@ -2,26 +2,97 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
-	gf "github.com/brianvoe/gofakeit/v6"
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+// pagination
+const PER_PAGE int = 10
+
+func Field(field string) string {
+	switch field {
+	case "name":
+		return gofakeit.Name()
+	case "phrase":
+		return gofakeit.Phrase()
+	}
+
+	return "incorrected type"
+}
+
+func Generator(endpoint map[string]interface{}) any {
+	scheme := endpoint["scheme"].(map[string]interface{})
+
+	if endpoint["response"].(string) == "single" {
+		single := make(map[string]interface{})
+		for key, value := range scheme {
+			single[key] = Field(value.(string))
+		}
+
+		return single
+	} else {
+		var per_page int = PER_PAGE
+
+		if count := endpoint["count"]; count != nil {
+			per_page = int(count.(float64))
+		}
+
+		generated := make([]map[string]interface{}, per_page)
+		for i := 0; i < per_page; i++ {
+			single := make(map[string]interface{})
+			for key, value := range scheme {
+				single[key] = Field(value.(string))
+			}
+			generated[i] = single
+		}
+
+		return generated
+	}
+
+}
+
 func main() {
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		scheme := map[string]interface{}{
-			"name":        gf.Name(),
-			"description": gf.Phrase(),
-		}
-		resp, err := json.Marshal(scheme)
-		if err != nil {
-			panic(err)
-		}
+	// set flags
+	var ConfigFile string
+	var Port int
 
-		return c.SendString((string(resp)))
+	var rootCmd = &cobra.Command{
+		Use:   "fake",
+		Short: "Create fake api with auto-generated response.",
+		Long:  "Create server with fake response data. Using config file fakeconfig.json",
+		Run: func(cmd *cobra.Command, args []string) {
+			viper.SetConfigFile(ConfigFile)
+			viper.ReadInConfig()
 
-	})
+			// create server instance
+			server := fiber.New()
 
-	app.Listen(":3000")
+			server.All("/*", func(c *fiber.Ctx) error {
+				// check current path
+				data := viper.Get(fmt.Sprintf("endpoints.%v", c.Params("*")))
+				if data == nil {
+					return c.SendStatus(404)
+				}
+
+				endpoint := data.(map[string]interface{})
+				resp, err := json.Marshal(Generator(endpoint))
+				if err != nil {
+					panic(err)
+				}
+
+				return c.SendString((string(resp)))
+			})
+
+			server.Listen(fmt.Sprintf(":%v", Port))
+		},
+	}
+
+	// run command with flags
+	rootCmd.Flags().StringVarP(&ConfigFile, "config", "c", "fake.json", "Config file for create server with schema & endpoints.")
+	rootCmd.Flags().IntVarP(&Port, "port", "p", 8765, "Running port.")
+	rootCmd.Execute()
 }
