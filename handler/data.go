e62bcd4c94dc90096c18f4155da0cf0e00bcbcb8
paqstd <zebartcoc@gmail.com>
2023-02-21 19:07:12 +0300

    Added some new types for response

--- handler/data.go
+++ handler/data.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/brianvoe/gofakeit/v6"
 )
 
@@ -8,22 +10,83 @@ func generateData(fields map[string]string) map[string]interface{} {
 	data := make(map[string]interface{})
 	for key, value := range fields {
 		switch value {
-		case "name":
-			data[key] = gofakeit.Name()
-		case "city":
-			data[key] = gofakeit.City()
-		case "email":
-			data[key] = gofakeit.Email()
+		// id
 		case "uuid":
 			data[key] = gofakeit.UUID()
+		// geography
+		case "city":
+			data[key] = gofakeit.City()
+		case "state":
+			data[key] = gofakeit.State()
+		case "country":
+			data[key] = gofakeit.Country()
+		case "latitude":
+			data[key] = gofakeit.Latitude()
+		case "longitude":
+			data[key] = gofakeit.Longitude()
+		// person
+		case "name":
+			data[key] = gofakeit.Name()
+		case "name_prefix":
+			data[key] = gofakeit.NamePrefix()
+		case "name_suffix":
+			data[key] = gofakeit.NameSuffix()
+		case "first_name":
+			data[key] = gofakeit.FirstName()
+		case "last_name":
+			data[key] = gofakeit.LastName()
+		case "gender":
+			data[key] = gofakeit.Gender()
+		case "ssn":
+			data[key] = gofakeit.SSN()
+		case "hobby":
+			data[key] = gofakeit.Hobby()
+		case "email":
+			data[key] = gofakeit.Email()
+		case "phone":
+			data[key] = gofakeit.Phone()
+		case "username":
+			data[key] = gofakeit.Username()
+		case "password":
+			data[key] = gofakeit.Password(true, true, true, true, true, 8)
+		// text
+		case "paragraph":
+			data[key] = gofakeit.Paragraph(5, 10, 3, "\n")
+		case "sentence":
+			data[key] = gofakeit.Sentence(5)
+		case "phrase":
+			data[key] = gofakeit.Phrase()
+		case "quote":
+			data[key] = gofakeit.Quote()
 		case "word":
 			data[key] = gofakeit.Word()
-		case "price":
-			data[key] = gofakeit.Price(0, 1000)
-		case "paragraph":
-			data[key] = gofakeit.Paragraph(3, 5, 8, "\n")
-		case "phrase":
-			data[key] = gofakeit.Sentence(5)
+		// data
+		case "date":
+			data[key] = gofakeit.Date()
+		case "second":
+			data[key] = gofakeit.Second()
+		case "minute":
+			data[key] = gofakeit.Minute()
+		case "hour":
+			data[key] = gofakeit.Hour()
+		case "month":
+			data[key] = gofakeit.Month()
+		case "day":
+			data[key] = gofakeit.Day()
+		case "year":
+			data[key] = gofakeit.Year()
+		// internet
+		case "url":
+			data[key] = gofakeit.URL()
+		case "domain":
+			data[key] = fmt.Sprintf("%s.%s", gofakeit.DomainName(), gofakeit.DomainSuffix())
+		// numbers
+		case "int":
+			data[key] = gofakeit.Int32()
+		case "float":
+			data[key] = gofakeit.Float32()
+		default:
+			data[key] = fmt.Sprintf("Unsupported field type: %s", value)
 		}
 	}
 	return data
